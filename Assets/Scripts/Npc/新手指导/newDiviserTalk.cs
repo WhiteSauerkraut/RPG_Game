ce24f421fe98c1751cb043277817a5884895a89d
WhiteSauerkraut <yangzhongyi2018@gmail.com>
2019-06-11 20:06:40 +0800

    更新了出生场景

---
+++ Assets/Scripts/Npc/新手指导/newDiviserTalk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newDiviserTalk : MonoBehaviour, InteractEvents
+{
+    public TalkManager talkWindow;
+    string[] TalkTexts;
+    string[] FightTexts;
+
+    void Start()
+    {
+        talkWindow = GameObject.FindGameObjectWithTag("Canvas").transform.Find("InteractSystem/TalkWindow")
+            .gameObject.GetComponent<TalkManager>();
+    }
+
+    public void Talk()
+    {
+        talkWindow.Show(TalkTexts, TalkNext);
+    }
+
+    public void Fight()
+    {
+        talkWindow.Show(FightTexts, FightNext);
+    }
+
+    void FightNext()
+    {
+
+    }
+
+    void TalkNext()
+    {
+        Debug.Log("Next");
+    }
+
+    public void Exchange()
+    {
+        TradeManager.Instance.ShowTradeWindow();
+    }
+
+}
